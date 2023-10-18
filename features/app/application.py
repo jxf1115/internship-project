f786366880f0cfb52e5642e6bb48fd0f060d1446
Julio <juliofernandez.re@gmail.com>
2023-10-18 15:52:01 -0700

    Corrected code Task2

---
+++ features/app/application.py
@@ -0,0 +1,12 @@
+from pages.base_page import Page
+from pages.main_page import MainPage
+from pages.header import Header
+from pages.signin_page import SignInPage
+
+
+class Application:
+
+    def __init__(self, driver):
+        self.main_page = MainPage(driver)
+        self.header = Header(driver)
+        self.signin_page = SignInPage(driver)
